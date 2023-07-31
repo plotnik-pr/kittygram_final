b75592a72cf79d04a27472a120a03a8ae6f876a9
plotnik-pr <tarronmorran@gmail.com>
2023-08-01 00:02:30 +0400

    Правки

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='11111')
 
 DEBUG = os.getenv('DEBUG', default = 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
                  
 INSTALLED_APPS = [
     'django.contrib.admin',
